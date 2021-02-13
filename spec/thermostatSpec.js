cf236c40fcc3840a4812611127b4f1e1b7f3c30b
HolsDuckett <hf.duckett@gmail.com>
2021-02-13 13:51:55 +0000

    amended code functionality to increase temp in increments of 1 instead of given argument amount

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -11,22 +11,39 @@ describe('Thermostat', function(){
     expect(thermostat.currentTemperature()).toEqual(20);
   });
 
-  it('increases the temperature', function() {
-    thermostat.up(1);
+  it('increases the temperature by 1 degree', function() {
+    thermostat.up();
     expect(thermostat.currentTemperature()).toEqual(21);
   });
 
-  it('decreases the tempreture', function() {
-    thermostat.down(1);
+  it('decreases the tempreture by 1 degree', function() {
+    thermostat.down();
     expect(thermostat.currentTemperature()).toEqual(19);
   });
 
-  it('raises an error when you go below min temp', function() {
-    expect(function(){thermostat.down(11);}).toThrowError("That is below the minimum temperature");
+  it('has a minimum tempreture of 10 degrees', function() {
+    expect(thermostat.MINIMUM_TEMPERATURE).toEqual(10)
   });
 
-  it('raises an error when you go above max temp', function() {
-    expect(function(){thermostat.up(6);}).toThrowError("That is above the maximum temperature");
+  it('raises an error when you go below minimum temperature', function() {
+      for (var i = 1; i < 11; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.currentTemperature()).toEqual(10)
+      expect(function(){
+        thermostat.down();
+      }).toThrowError("Minimum temperature reached");
+  });
+
+
+  it('raises an error when you reach the maximum temperature, when power saving mode is on', function() {
+    for (var j = 0; j < 5; j++) {
+      thermostat.up();
+    }
+    expect(thermostat.currentTemperature()).toEqual(25)
+    expect(function(){
+      thermostat.up();
+    }).toThrowError("Maximum temperature reached");
   });
 
   it('defaults as saving power mode as on', function() {
@@ -43,34 +60,45 @@ describe('Thermostat', function(){
     expect(thermostat.currentPowerSavingMode()).toBe(true);
   });
 
-  it('should raise the max temp to 32 when PSM is off', function() {
-    thermostat.powerModeOff()
-    thermostat.up(6)
-    expect(thermostat.currentTemperature()).toEqual(26)
-  });
-
   it('should be able to change the temperature to 20 with a reset function', function() {
-    thermostat.up(5)
+    thermostat.up()
+    expect(thermostat.currentTemperature()).toEqual(21);
     thermostat.reset()
     expect(thermostat.currentTemperature()).toEqual(20);
   });
 
-  it('should return low energy usage if temperature is below 18', function () {
-    thermostat.down(5)
-    expect(thermostat.energyUsage()).toEqual("This temperature is low usage");
+
+  it('should raise the max temp to 32 when power saving mode is off', function() {
+    thermostat.powerModeOff()
+    expect(thermostat.currentPowerSavingMode()).toBe(false);
+    for (var j = 0; j < 12; j++) {
+      thermostat.up();
+    }
+    expect(thermostat.currentTemperature()).toEqual(32)
+    expect(function(){
+      thermostat.up();
+    }).toThrowError('Maximum temperature reached')
   });
 
-  it('should return medium energy usage if temperature is above 18 and below or equal 25', function() {
-    expect(thermostat.energyUsage()).toEqual("This temperature is medium usage");
-  });
+  describe('Display energy usage levels', function(){
+    it('will display low energy usage if temperature is below 18', function () {
+      for (var i = 1; i < 3; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual("This temperature is low usage");
+    });
 
-  it('should return high energy usage if temperature is above 25', function() {
-    thermostat.powerModeOff();
-    thermostat.up(6);
-    expect(thermostat.energyUsage()).toEqual("This temperature is high usage");
-  });
+    it('will display medium energy usage if temperature is above 18 and below or equal 25', function() {
+      expect(thermostat.energyUsage()).toEqual("This temperature is medium usage");
+    });
 
-
-
+    it('should return high energy usage if temperature is above 25', function() {
+      thermostat.powerModeOff();
+      for (var i = 1; i < 6 ; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual("This temperature is high usage");
+    });
+  }); // describe usage end
 
 }); // class end
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,6 +6,7 @@ class Thermostat {
     this.temperature = 20;
     this.powerSavingMode = true;
     this.maxTemp = 25;
+    this.MINIMUM_TEMPERATURE = 10
   };
 
   currentTemperature() {
@@ -16,18 +17,18 @@ class Thermostat {
     return this.powerSavingMode;
   };
 
-  up(amount) {
-    if (this.temperature + amount > this.maxTemp) {
-      throw new Error('That is above the maximum temperature');
+  up() {
+    if (this.temperature + 1 > this.maxTemp) {
+      throw new Error('Maximum temperature reached');
     };
-    this.temperature += amount;
+    this.temperature += 1;
   };
 
-  down(amount) {
-    if (this.temperature - amount < 10) {
-      throw new Error('That is below the minimum temperature');
+  down() {
+    if (this.temperature - 1 < this.MINIMUM_TEMPERATURE) {
+      throw new Error('Minimum temperature reached');
     };
-    this.temperature -= amount;
+    this.temperature -= 1;
   };
 
   powerModeOff() {
@@ -45,10 +46,10 @@ class Thermostat {
   };
 
   energyUsage() {
-    if (this.temperature < 18) {
+    if (this.temperature <= 18) {
       return "This temperature is low usage";
     };
-    if (this.temperature > 18 && this.temperature <= 25) {
+    if (this.temperature > 18 && this.temperature < 25) {
       return "This temperature is medium usage";
     };
     return "This temperature is high usage";
